128119d8f22e2b2131430ccbda2852956cfd4e0a
Ian Rash <redcodefinal@gmail.com>
2017-04-09 10:52:51 -0700

    waveform fixes

--- src/world_gen/passes/waveforms/sine_wave.cr
+++ src/world_gen/passes/waveforms/sine_wave.cr
@@ -13,8 +13,8 @@ class SineWave < Pass
     elsif axis == :y
       height = (Math.sin(two_pi * (y - world.y_range.begin)/world.y_range.size * 4.0)) * (world.z_range.size/3.0) + (world.z_range.size/3.0)      
     elsif axis == :xy
-      x_height = (Math.sin(two_pi * (x - x_range.begin)/x_range.size * 4.0)) * (z_range.size/3.0) + (z_range.size/3.0)
-      y_height = (Math.sin(two_pi * (y - y_range.begin)/y_range.size * 4.0)) * (z_range.size/3.0) + (z_range.size/3.0)
+      x_height = (Math.sin(two_pi * (x - world.x_range.begin)/world.x_range.size * 4.0)) * (world.z_range.size/3.0) + (world.z_range.size/3.0)
+      y_height = (Math.sin(two_pi * (y - world.y_range.begin)/world.y_range.size * 4.0)) * (world.z_range.size/3.0) + (world.z_range.size/3.0)
 
       max = [x_height, y_height].max
       min = [x_height, y_height].min
